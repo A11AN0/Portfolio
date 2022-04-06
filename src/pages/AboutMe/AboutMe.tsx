24856ca0a1b0f31e4d63f0cc6909b4850dbc7327
Allan <matrixboston7613@gmail.com>
2022-04-06 23:14:38 +0800

    about me title animated for smooth transition when navbar extends, responsivity

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import './AboutMe.scss'
+import React, {useEffect} from 'react';
+import './AboutMe.scss';
+import gsap from "gsap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
-const AboutMe = (props:any) => {
-  return (
-    <section className="about-me" id="about">
-        <h1 className="about-me__title">
-            About Me.
-        </h1>
-        <div className="about-me__h-container">
-          <div className="about-me__h-container__description">
-            <div className="about-me__h-container__description__text">
-            I'm an Australian trainee developer undertaking a self-funded software development program with _Nology. 
-            Highly driven, adaptable, personable, team-oriented  and experienced in React, 
-            Javascript, node.js and HTML5 among various other essential elements of software design (eventually lol). I will go above and beyond 
-            to deliver a polished and functional product for customer and corporation alike .
-            </div>
-          </div>
-          <div className="about-me__h-container__tech-stack">
-            <div className="about-me__h-container__tech-stack__box">
-              <h4 className="about-me__h-container__tech-stack__box__title">& My Tech Stack</h4>
-              <div>hi</div>
-              <div>hi</div>
-              <div>hi</div>
-              <div>hi</div>
-              <div>hi</div>
-              <div>hi</div>
-              <div>hi</div>
-            </div>
-          </div>
-        </div>  
-      </section>
-  )
+
+type AppProps = {
+  isNavExtended: boolean;
 }
 
-export default AboutMe
+const AboutMe = ({isNavExtended}:AppProps) => {
+  
+  useEffect(() => {
+    isNavExtended? 
+    gsap.to(".about-me__title", { marginLeft: '16rem', duration: 0.5})
+    :
+    gsap.to(".about-me__title", { marginLeft: '0', duration: 0.5})
+    
+  }, [isNavExtended]);
+
+
+  return (
+    <section className="about-me" id="about">
+        <div className="about-me__title">
+          <FontAwesomeIcon icon={faChevronLeft} className="about-me__title__arrow"/>
+            About Me.
+          <FontAwesomeIcon icon={faChevronRight} className="about-me__title__arrow"/>
+        </div> 
+        
+        
+    </section>
+  );
+};
+
+export default AboutMe;

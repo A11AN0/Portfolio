b9f8581c465b7b07ed09693c8c787eb945da2e2c
Allan <matrixboston7613@gmail.com>
2022-04-08 04:59:35 +0800

    componentd for About me page sections

---
+++ src/pages/AboutMe/Skills/Skills.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Skills.scss';
+
+
+
+type AppProps = {
+  isNavExtended: boolean;
+}
+
+const Skills = ({isNavExtended}:AppProps) => {
+
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default Skills;
---
+++ src/pages/AboutMe/TechStack/TechStack.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TechStack.scss';
+
+
+
+type AppProps = {
+  isNavExtended: boolean;
+}
+
+const TechStack = ({isNavExtended}:AppProps) => {
+
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default TechStack;

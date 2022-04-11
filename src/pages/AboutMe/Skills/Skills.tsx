e637651443f64761e79d7ef81f939180d1815636
Allan <matrixboston7613@gmail.com>
2022-04-11 22:21:04 +0800

    MySkills section added to aboutMe page

--- src/pages/AboutMe/Skills/Skills.tsx
+++ src/pages/AboutMe/Skills/Skills.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Skills.scss';
+import gsap from "gsap";
 
 
 
@@ -9,10 +10,19 @@ type AppProps = {
 
 const Skills = ({isNavExtended}:AppProps) => {
 
+  //Page width upon nav extension
+  useEffect(() => {
+    isNavExtended
+      ? gsap.to(".skills", { marginLeft: "16rem", duration: 0.5 })
+      : gsap.to(".skills", { marginLeft: "0", duration: 0.5 });
+  }, [isNavExtended]);
+
 
   return (
-    <>
-    </>
+    <div className="skills">
+      <div className="skills__top"></div>
+      <div className="skills__title">& My Skills</div>
+    </div>
   );
 };
 

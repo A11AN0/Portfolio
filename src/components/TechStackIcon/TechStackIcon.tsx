2a2e5a2951e04de00d954e942dedb2c4a69d2e62
Allan <matrixboston7613@gmail.com>
2022-04-11 22:18:20 +0800

    TechstackIcons component icon/text template

---
+++ src/components/TechStackIcon/TechStackIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+
+
+type AppProps = {
+  techName:String,
+  color:String,
+  icon: any
+};
+
+const TechStackIcon = ({ techName, icon, color }: AppProps) => {
+  return (
+    <div className="tech-stack__items__icon">
+      <FontAwesomeIcon icon={icon} style={{color: `${color}`}}/>
+      <p className="tech-stack__items__icon__text">{techName}</p>
+    </div>
+  );
+};
+
+export default TechStackIcon;
---
+++ src/components/TechStackIcon/index.ts
@@ -0,0 +1,2 @@
+import TechStackIcon from "./TechStackIcon";
+export default TechStackIcon;

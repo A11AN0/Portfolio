30e60516a039c23b9878ff40bcb13152a1800047
Allan <matrixboston7613@gmail.com>
2022-04-11 22:20:27 +0800

    Formatting

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,50 +1,57 @@
-import React, {useEffect, useRef} from 'react';
-import './AboutMe.scss';
+import React, { useEffect, useRef } from "react";
+import "./AboutMe.scss";
 import gsap from "gsap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft} from '@fortawesome/free-solid-svg-icons';
-import Description from './Description/Description';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faChevronRight, faChevronLeft} from "@fortawesome/free-solid-svg-icons";
+import Description from "./Description/Description";
+import TechStack from "./TechStack/TechStack";
+import Skills from "./Skills/Skills";
 
 type AppProps = {
   isNavExtended: boolean;
-}
+};
 
-const AboutMe = ({isNavExtended}:AppProps) => {
-  
+const AboutMe = ({ isNavExtended }: AppProps) => {
   //Behavior of the title when nav is extended
   useEffect(() => {
-    isNavExtended? 
-    gsap.to(".about-me__title", { marginLeft: '16rem', duration: 0.5})
-    :
-    gsap.to(".about-me__title", { marginLeft: '0', duration: 0.5})
-    
+    isNavExtended
+      ? gsap.to(".about-me__title", { marginLeft: "16rem", duration: 0.5 })
+      : gsap.to(".about-me__title", { marginLeft: "0", duration: 0.5 });
   }, [isNavExtended]);
 
   //So that the container will scroll to the center div upon mounting
-  const scrollContainer:any = useRef();
+  const scrollContainer: any = useRef();
   useEffect(() => {
     scrollContainer.current.scrollTo(window.innerWidth, 0);
   }, []);
 
-
   return (
     <section className="about-me" id="about">
-        <div className="about-me__title">
-          <FontAwesomeIcon icon={faChevronLeft} className="about-me__title__arrow"/>
-            About Me.
-          <FontAwesomeIcon icon={faChevronRight} className="about-me__title__arrow"/>
-        </div>
+      <div className="about-me__title">
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          className="about-me__title__arrow"
+        />
+        About Me.
+        <FontAwesomeIcon
+          icon={faChevronRight}
+          className="about-me__title__arrow"
+        />
+      </div>
 
-        <div className="about-me__contents" ref={scrollContainer}>
-          <div className="about-me__contents__carousel">
-            <div className="about-me__contents__carousel__page about-me__contents__carousel__page--skills"></div>
-            <div className="about-me__contents__carousel__page about-me__contents__carousel__page--description"><Description isNavExtended={isNavExtended}/></div>
-            <div className="about-me__contents__carousel__page about-me__contents__carousel__page--techstack"></div>
+      <div className="about-me__contents" ref={scrollContainer}>
+        <div className="about-me__contents__carousel">
+          <div className="about-me__contents__carousel__page about-me__contents__carousel__page--skills">
+            <Skills isNavExtended={isNavExtended} />
           </div>
-        </div>  
-        
-        
+          <div className="about-me__contents__carousel__page about-me__contents__carousel__page--description">
+            <Description isNavExtended={isNavExtended} />
+          </div>
+          <div className="about-me__contents__carousel__page about-me__contents__carousel__page--techstack">
+            <TechStack isNavExtended={isNavExtended} />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };

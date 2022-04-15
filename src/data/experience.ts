770a7fc9b97b8181a318f47b248e66dd20cf491c
Allan <matrixboston7613@gmail.com>
2022-04-16 03:06:42 +0800

    experience/skill data containing parameters to be fed into generateSkillsChart function

---
+++ src/data/experience.ts
@@ -0,0 +1,87 @@
+import {
+    faMeteor,
+    faLineChart,
+    faPuzzlePiece,
+    faMicroscope,
+    faFlaskVial,
+    faServer,
+    faPeopleLine,
+    faMobileAlt,
+    faParagraph,
+} from "@fortawesome/free-solid-svg-icons";
+
+type experienceData = {
+    experience: string;
+    icon: any;
+    primaryColor: string;
+    secondaryColor: string;
+    percentage: number;
+};
+
+const experienceListData: experienceData[] = [
+    {
+        experience: "Agile Scrum Methodology",
+        icon: faMeteor,
+        primaryColor: "#e91e63",
+        secondaryColor: "#673ab7",
+        percentage: 100,
+    },
+    {
+        experience: "Data Analysis",
+        icon: faLineChart,
+        primaryColor: "#0fdb83",
+        secondaryColor: "#0591e3",
+        percentage: 100,
+    },
+    {
+        experience: "Object Oriented Programming",
+        icon: faPuzzlePiece,
+        primaryColor: "#fbff00",
+        secondaryColor: "#e30505",
+        percentage: 100,
+    },
+    {
+        experience: "Research",
+        icon: faMicroscope,
+        primaryColor: "#e91e63",
+        secondaryColor: "#673ab7",
+        percentage: 100,
+    },
+    {
+        experience: "Test Driven Development",
+        icon: faFlaskVial,
+        primaryColor: "#0fdb83",
+        secondaryColor: "#0591e3",
+        percentage: 100,
+    },
+    {
+        experience: "REST API Design",
+        icon: faServer,
+        primaryColor: "#fbff00",
+        secondaryColor: "#e30505",
+        percentage: 100,
+    },
+    {
+        experience: "Collaborative Interpersonal Development",
+        icon: faPeopleLine,
+        primaryColor: "#e91e63",
+        secondaryColor: "#673ab7",
+        percentage: 100,
+    },
+    {
+        experience: "Mobile First Design",
+        icon: faMobileAlt,
+        primaryColor: "#0fdb83",
+        secondaryColor: "#0591e3",
+        percentage: 100,
+    },
+    {
+        experience: "Technical Documentation",
+        icon: faParagraph,
+        primaryColor: "#fbff00",
+        secondaryColor: "#e30505",
+        percentage: 100,
+    },
+];
+
+export default experienceListData;

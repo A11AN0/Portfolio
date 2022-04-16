76a184575183a0c278d4b76a089bfece93c1a9c6
Allan <matrixboston7613@gmail.com>
2022-04-16 22:57:36 +0800

    responsivity, formatting and footer for redirection arrow to be placed

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -2,58 +2,73 @@ import React, { useEffect, useRef } from "react";
 import "./AboutMe.scss";
 import gsap from "gsap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faChevronRight, faChevronLeft} from "@fortawesome/free-solid-svg-icons";
+import {
+    faChevronRight,
+    faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
 import Description from "./Description/Description";
 import TechStack from "./TechStack/TechStack";
 import Skills from "./Skills/Skills";
 
 type AppProps = {
-  isNavExtended: boolean;
+    isNavExtended: boolean;
 };
 
 const AboutMe = ({ isNavExtended }: AppProps) => {
-  //Behavior of the title when nav is extended
-  useEffect(() => {
-    isNavExtended
-      ? gsap.to(".about-me__title", { marginLeft: "16rem", duration: 0.5 })
-      : gsap.to(".about-me__title", { marginLeft: "0", duration: 0.5 });
-  }, [isNavExtended]);
+    //Behavior of the title when nav is extended
+    useEffect(() => {
+        isNavExtended
+            ? gsap.to(".about-me__shiftingElement", {
+                  marginLeft: "16rem",
+                  duration: 0.5,
+              })
+            : gsap.to(".about-me__shiftingElement", {
+                  marginLeft: "0",
+                  duration: 0.5,
+              });
+    }, [isNavExtended]);
 
-  //So that the container will scroll to the center div upon mounting
-  const scrollContainer: any = useRef();
-  useEffect(() => {
-    scrollContainer.current.scrollTo(window.innerWidth, 0);
-  }, []);
+    //So that the container will scroll to the center div upon mounting
+    const scrollContainer: any = useRef();
+    useEffect(() => {
+        scrollContainer.current.scrollTo(window.innerWidth, 0);
+    }, []);
 
-  return (
-    <section className="about-me" id="about">
-      <div className="about-me__title">
-        <FontAwesomeIcon
-          icon={faChevronLeft}
-          className="about-me__title__arrow"
-        />
-        About Me.
-        <FontAwesomeIcon
-          icon={faChevronRight}
-          className="about-me__title__arrow"
-        />
-      </div>
+    return (
+        <section className="about-me" id="about">
+            <div className="about-me__title about-me__shiftingElement ">
+                <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    className="about-me__title__arrow"
+                    style={{ display: isNavExtended ? "none" : "initial" }}
+                />
+                About Me.
+                <FontAwesomeIcon
+                    icon={faChevronRight}
+                    className="about-me__title__arrow"
+                    style={{ display: isNavExtended ? "none" : "initial" }}
+                />
+            </div>
 
-      <div className="about-me__contents" ref={scrollContainer}>
-        <div className="about-me__contents__carousel">
-          <div className="about-me__contents__carousel__page about-me__contents__carousel__page--skills">
-            <Skills isNavExtended={isNavExtended} />
-          </div>
-          <div className="about-me__contents__carousel__page about-me__contents__carousel__page--description">
-            <Description isNavExtended={isNavExtended} />
-          </div>
-          <div className="about-me__contents__carousel__page about-me__contents__carousel__page--techstack">
-            <TechStack isNavExtended={isNavExtended} />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+            <div className="about-me__contents" ref={scrollContainer}>
+                <div className="about-me__contents__carousel">
+                    <div className="about-me__contents__carousel__page about-me__contents__carousel__page--skills">
+                        <Skills isNavExtended={isNavExtended} />
+                    </div>
+                    <div className="about-me__contents__carousel__page about-me__contents__carousel__page--description">
+                        <Description isNavExtended={isNavExtended} />
+                    </div>
+                    <div className="about-me__contents__carousel__page about-me__contents__carousel__page--techstack">
+                        <TechStack isNavExtended={isNavExtended} />
+                    </div>
+                </div>
+            </div>
+            <footer className="about-me__arrow-down about-me__shiftingElement">
+                {" "}
+                this is the arrow
+            </footer>
+        </section>
+    );
 };
 
 export default AboutMe;

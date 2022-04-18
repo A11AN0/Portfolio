655f4fb485733416a7d7fcbd6e0d659dbe8d8bc4
Allan <matrixboston7613@gmail.com>
2022-04-18 23:21:08 +0800

    passed setElementInViewport down as props to all about me pages, and upon setting element in viewport, visibililty of the prompts and their arrows is changed accordingly, through gsap and useState; will require refactoring

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useEffect, useRef } from "react";
+import React, { SyntheticEvent, useEffect, useRef, useState } from "react";
 import "./AboutMe.scss";
 import gsap from "gsap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -17,6 +17,62 @@ type AppProps = {
 
 const generateBlank = "  ";
 const AboutMe = ({ isNavExtended }: AppProps) => {
+    //Procedure for setting the titles in the arrow prompt for each section
+    const [elementInViewPort, setElementInViewPort] = useState("description"); //will alert the user when the element is in viewport
+    const handlePromptVisibility = (visibleElement: string) => {
+        const timeline = gsap.timeline();
+        switch (visibleElement) {
+            case "description":
+                timeline.to(".about-me__title__prompt--right", {
+                    visibility: "visible",
+                    duration: "instant",
+                });
+                timeline.to(".about-me__title__arrow--right", {
+                    visibility: "visible",
+                    duration: "instant",
+                });
+                timeline.to(".about-me__title__prompt--left", {
+                    visibility: "visible",
+                    duration: "instant",
+                });
+                timeline.to(".about-me__title__arrow--left", {
+                    visibility: "visible",
+                    duration: "instant",
+                });
+
+                break;
+            case "skills":
+                //make left arrow and right prompt invisible
+                timeline.to(".about-me__title__prompt--left", {
+                    visibility: "hidden",
+                    duration: "instant",
+                });
+                timeline.to(".about-me__title__arrow--left", {
+                    visibility: "hidden",
+                    duration: "instant",
+                });
+                break;
+            case "techstack":
+                //make right arrow invisible
+                timeline.to(".about-me__title__prompt--right", {
+                    visibility: "hidden",
+                    duration: "instant",
+                });
+                timeline.to(".about-me__title__arrow--right", {
+                    visibility: "hidden",
+                    duration: "instant",
+                });
+                break;
+
+            default:
+                return;
+        }
+    };
+
+    useEffect(() => {
+        handlePromptVisibility(elementInViewPort);
+    }, [elementInViewPort]);
+
     //Procedure to animate the arrow
     const animateArrow = () => {
         const arrowAnimationTimeline = gsap.timeline();
@@ -97,29 +153,54 @@ const AboutMe = ({ isNavExtended }: AppProps) => {
     return (
         <section className="about-me" id="about">
             <div className="about-me__title about-me__shiftingElement ">
+                <p
+                    className="about-me__title__prompt about-me__title__prompt--left"
+                    style={{ display: isNavExtended ? "none" : "initial" }}
+                >
+                    {elementInViewPort === "description"
+                        ? "Experience"
+                        : "In a Nutshell"}
+                </p>
                 <FontAwesomeIcon
                     icon={faChevronLeft}
-                    className="about-me__title__arrow"
+                    className="about-me__title__arrow about-me__title__arrow--left"
                     style={{ display: isNavExtended ? "none" : "initial" }}
                 />
-                About Me.
+                <p className="about-me__title__main">About Me.</p>
                 <FontAwesomeIcon
                     icon={faChevronRight}
-                    className="about-me__title__arrow"
+                    className="about-me__title__arrow about-me__title__arrow--right"
                     style={{ display: isNavExtended ? "none" : "initial" }}
                 />
+                <p
+                    className="about-me__title__prompt about-me__title__prompt--right "
+                    style={{ display: isNavExtended ? "none" : "initial" }}
+                >
+                    {elementInViewPort === "description"
+                        ? "Tech Stack"
+                        : "In a Nutshell"}
+                </p>
             </div>
 
             <div className="about-me__contents" ref={scrollContainer}>
                 <div className="about-me__contents__carousel">
                     <div className="about-me__contents__carousel__page about-me__contents__carousel__page--skills">
-                        <Skills isNavExtended={isNavExtended} />
+                        <Skills
+                            isNavExtended={isNavExtended}
+                            setElementInViewPort={setElementInViewPort}
+                        />
                     </div>
                     <div className="about-me__contents__carousel__page about-me__contents__carousel__page--description">
-                        <Description isNavExtended={isNavExtended} />
+                        <Description
+                            isNavExtended={isNavExtended}
+                            setElementInViewPort={setElementInViewPort}
+                        />
                     </div>
                     <div className="about-me__contents__carousel__page about-me__contents__carousel__page--techstack">
-                        <TechStack isNavExtended={isNavExtended} />
+                        <TechStack
+                            isNavExtended={isNavExtended}
+                            setElementInViewPort={setElementInViewPort}
+                        />
                     </div>
                 </div>
             </div>

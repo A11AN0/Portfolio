32d783c852e00d4c5f08ffd56b339ce3d358e59e
Allan <matrixboston7613@gmail.com>
2022-04-20 23:32:45 +0800

    created project data file, for testing project generator and project cards; will contain data on projects, to be used for project cards

---
+++ src/data/projects.ts
@@ -0,0 +1,35 @@
+export type ProjectData = {
+    projectName: string;
+};
+
+const projectListData: ProjectData[] = [
+    {
+        projectName: "1",
+    },
+    {
+        projectName: "2",
+    },
+    {
+        projectName: "3",
+    },
+    {
+        projectName: "4",
+    },
+    {
+        projectName: "5",
+    },
+    {
+        projectName: "6",
+    },
+    {
+        projectName: "7",
+    },
+    {
+        projectName: "8",
+    },
+    {
+        projectName: "9",
+    },
+];
+
+export default projectListData;

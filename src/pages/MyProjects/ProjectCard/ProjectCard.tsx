89f6e611a45548aba42f409cfdd2be3d5ce8f9b0
Allan <matrixboston7613@gmail.com>
2022-04-21 21:46:21 +0800

    project card for displaying project info; general layout of card confirmed, now need to make recyclable

--- src/pages/MyProjects/ProjectCard/ProjectCard.tsx
+++ src/pages/MyProjects/ProjectCard/ProjectCard.tsx
@@ -3,6 +3,12 @@ import "./ProjectCard.scss";
 import { ProjectData } from "./../../../data/projects";
 import { useEffect } from "react";
 import gsap from "gsap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faJsSquare,
+    faHtml5,
+    faPython,
+} from "@fortawesome/free-brands-svg-icons";
 
 type AppProps = {
     project?: ProjectData;
@@ -26,7 +32,38 @@ const ProjectCard = ({ project, blank, isNavExtended }: AppProps) => {
         <div className="projectCard"></div>
     ) : (
         <div className="projectCard">
-            <p>{project!.projectName}</p>
+            <div className="projectCard__container">
+                <p className="projectCard__container__title">
+                    {project!.projectName}
+                </p>
+                <p className="projectCard__container__description">
+                    A good-old, classic, browser run game of snake made with
+                    vanilla Javascript jndsjfdkfjn hfbsjnskjfnfjnfnjn
+                </p>
+                <div className="projectCard__container__links">
+                    <div className="projectCard__container__links__item">
+                        <FontAwesomeIcon icon={faPython} />
+                        <p className="projectCard__container__links__item__text">
+                            Codebase
+                        </p>
+                    </div>
+                    <div className="projectCard__container__links__item">
+                        <FontAwesomeIcon icon={faJsSquare} />
+                        <p className="projectCard__container__links__item__text">
+                            Site
+                        </p>
+                    </div>
+                </div>
+                <div className="projectCard__container__tools">
+                    <FontAwesomeIcon icon={faJsSquare} />
+
+                    <FontAwesomeIcon icon={faHtml5} />
+
+                    <FontAwesomeIcon icon={faPython} />
+
+                    <FontAwesomeIcon icon={faPython} />
+                </div>
+            </div>
         </div>
     );
 };

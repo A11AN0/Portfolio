d968a0e818b46b2a1f35511d1d761f430d9a04a3
Allan <matrixboston7613@gmail.com>
2022-04-22 01:36:55 +0800

    added responsivity and styling for card, final style more or less finalised, reusable for project data array, will need to refactor

--- src/pages/MyProjects/ProjectCard/ProjectCard.tsx
+++ src/pages/MyProjects/ProjectCard/ProjectCard.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { SyntheticEvent, useEffect, useState } from "react";
 import "./ProjectCard.scss";
 import { ProjectData } from "./../../../data/projects";
-import { useEffect } from "react";
 import gsap from "gsap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faJsSquare,
     faHtml5,
     faPython,
+    faGithubSquare,
+    faReact,
 } from "@fortawesome/free-brands-svg-icons";
+import { faExternalLinkSquare } from "@fortawesome/free-solid-svg-icons";
+import Tilt from "react-parallax-tilt";
 
 type AppProps = {
     project?: ProjectData;
@@ -28,43 +31,97 @@ const ProjectCard = ({ project, blank, isNavExtended }: AppProps) => {
               });
     }, [isNavExtended]);
 
+    const [isHovered, setIsHovered] = useState(false);
+
+    //Procedure to animate the project card when hovered
+    const specificClassName = project?.projectName.replace(/\s/g, "");
+    const animateHover = (event: string) => {
+        const hoverTimeline = gsap.timeline();
+
+        hoverTimeline.to(`.${specificClassName}`, {
+            opacity: event === "mouseenter" ? 1 : 0,
+            duration: 0.2,
+        });
+    };
+
+    //Procedure to handle all processes that occur when card is hovered over
+    const handleCardHover = (event: SyntheticEvent) => {
+        const action = event.type;
+        if (!(action === "mouseenter" || action === "mouseleave")) return;
+        setIsHovered(action === "mouseenter");
+        animateHover(action);
+    };
+
     return blank ? (
         <div className="projectCard"></div>
     ) : (
-        <div className="projectCard">
-            <div className="projectCard__container">
-                <p className="projectCard__container__title">
-                    {project!.projectName}
-                </p>
-                <p className="projectCard__container__description">
-                    A good-old, classic, browser run game of snake made with
-                    vanilla Javascript jndsjfdkfjn hfbsjnskjfnfjnfnjn
-                </p>
-                <div className="projectCard__container__links">
-                    <div className="projectCard__container__links__item">
-                        <FontAwesomeIcon icon={faPython} />
-                        <p className="projectCard__container__links__item__text">
-                            Codebase
+        <Tilt transitionSpeed={5000}>
+            <div
+                className="projectCard"
+                onMouseEnter={handleCardHover}
+                onMouseLeave={handleCardHover}
+            >
+                <div
+                    className={`projectCard__container ${
+                        isHovered && `projectCard__container--hovered`
+                    }`}
+                >
+                    <p className="projectCard__container__title">
+                        {project!.projectName}
+                    </p>
+                    <div
+                        className={`projectCard__container__info ${specificClassName}`}
+                    >
+                        <p className="projectCard__container__info__description">
+                            A good-old, classic, browser run game of snake made
+                            with vanilla Javascript jndsjfdkfjn
+                            hfbsjnskjfnfjnfnjn
                         </p>
+                        <div className="projectCard__container__info__links">
+                            <div className="projectCard__container__info__links__item">
+                                <FontAwesomeIcon
+                                    className="projectCard__container__info__links__item__svg"
+                                    icon={faGithubSquare}
+                                />
+                                <p className="projectCard__container__info__links__item__text">
+                                    Codebase
+                                </p>
+                            </div>
+                            <div className="projectCard__container__info__links__item">
+                                <FontAwesomeIcon
+                                    className="projectCard__container__info__links__item__svg"
+                                    icon={faExternalLinkSquare}
+                                />
+                                <p className="projectCard__container__info__links__item__text">
+                                    Site
+                                </p>
+                            </div>
+                        </div>
+                        <div className="projectCard__container__info__tools">
+                            <FontAwesomeIcon
+                                icon={faJsSquare}
+                                style={{ color: "#fbff26" }}
+                            />
+
+                            <FontAwesomeIcon
+                                icon={faHtml5}
+                                style={{ color: "#ff6e26" }}
+                            />
+
+                            <FontAwesomeIcon
+                                icon={faPython}
+                                style={{ color: "#7bff00" }}
+                            />
+
+                            <FontAwesomeIcon
+                                icon={faReact}
+                                style={{ color: "#1fd2ff" }}
+                            />
+                        </div>
                     </div>
-                    <div className="projectCard__container__links__item">
-                        <FontAwesomeIcon icon={faJsSquare} />
-                        <p className="projectCard__container__links__item__text">
-                            Site
-                        </p>
-                    </div>
-                </div>
-                <div className="projectCard__container__tools">
-                    <FontAwesomeIcon icon={faJsSquare} />
-
-                    <FontAwesomeIcon icon={faHtml5} />
-
-                    <FontAwesomeIcon icon={faPython} />
-
-                    <FontAwesomeIcon icon={faPython} />
                 </div>
             </div>
-        </div>
+        </Tilt>
     );
 };
 
